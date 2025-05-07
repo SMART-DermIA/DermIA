5ae89d9acccbcd8d1d9c18fb19fb01e3c7278ff3
Santfogu <santiforerog24@hotmail.com>
2025-05-07 17:00:32 +0200

    Historique v2

--- front/dermIa/src/historique/historique.jsx
+++ front/dermIa/src/historique/historique.jsx
@@ -15,12 +15,26 @@ const Historique = () => {
                         <h1 className="historique-title">Vos albums</h1>
                         <p className="historique-subtitle">Suivez l'évolution de chacun de vos grains de beauté</p>
                     </div>
-                    <Link to="/userAccueil" className="confirm-button" >
+                    <div className="historique-header-button">
+                    <Link to="/userAccueil" className="nouvelle-button" >
                         <LuScanSearch size={20} style={{ marginBottom: '.2em', marginRight: '.5em' }} />
-                        Lancer l’analyse
+                        Nouvelle analyse
                     </Link>
+                    </div>
                 </div>
                 <div className="historique-albums">
+                    <AlbumCard imageUrl="/image1.png" title="Album 1" lastModified="2023-10-01" />
+                    <AlbumCard imageUrl="/image2.png" title="Album 2" lastModified="2023-10-02" />
+                    <AlbumCard imageUrl="/image3.png" title="Album 3" lastModified="2023-10-03" />
+                    <AlbumCard imageUrl="/image1.png" title="Album 1" lastModified="2023-10-01" />
+                    <AlbumCard imageUrl="/image2.png" title="Album 2" lastModified="2023-10-02" />
+                    <AlbumCard imageUrl="/image3.png" title="Album 3" lastModified="2023-10-03" />
+                    <AlbumCard imageUrl="/image1.png" title="Album 1" lastModified="2023-10-01" />
+                    <AlbumCard imageUrl="/image2.png" title="Album 2" lastModified="2023-10-02" />
+                    <AlbumCard imageUrl="/image3.png" title="Album 3" lastModified="2023-10-03" />
+                    <AlbumCard imageUrl="/image1.png" title="Album 1" lastModified="2023-10-01" />
+                    <AlbumCard imageUrl="/image2.png" title="Album 2" lastModified="2023-10-02" />
+                    <AlbumCard imageUrl="/image3.png" title="Album 3" lastModified="2023-10-03" />
                     <AlbumCard imageUrl="/image1.png" title="Album 1" lastModified="2023-10-01" />
                     <AlbumCard imageUrl="/image2.png" title="Album 2" lastModified="2023-10-02" />
                     <AlbumCard imageUrl="/image3.png" title="Album 3" lastModified="2023-10-03" />
