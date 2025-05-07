ef87319b9f586ef64783955e3ec200bd775ff483
Santfogu <santiforerog24@hotmail.com>
2025-05-07 09:03:39 +0200

    Home v3

--- front/dermIa/src/home/home.jsx
+++ front/dermIa/src/home/home.jsx
@@ -9,20 +9,24 @@ const Home = () => {
       <div className="btn-container">
       <button className="button">Commencer</button>
       </div>
+      <img src="/image1.png" alt="Foto Finder" className='image' />
       <h2 className="features-title">Découvrez nos fonctionnalités innovantes pour la santé de votre peau.</h2>
       <div className="features-section">
           
           <div className="feature">
+          <img src="/image2.png" alt="-" className='image' />
             <h3 className="feature-title">Suivez l'évolution de vos grains de beauté facilement et efficacement.</h3>
             <p className="feature-text">Notre IA détecte la dangerosité de vos grains de beauté en un clin d'œil.</p>
           </div>
 
           <div className="feature">
+          <img src="/image3.png" alt="-" className='image' />
             <h3 className="feature-title">Gérez vos grains de beauté avec notre interface intuitive et simple.</h3>
             <p className="feature-text">Créez des albums pour suivre l'évolution de chaque grain de beauté.</p>
           </div>
 
           <div className="feature">
+          <img src="/image4.png" alt="-" className='image' />
             <h3 className="feature-title">Utilisez la fonction drag & drop pour une expérience utilisateur fluide.</h3>
             <p className="feature-text">Déplacez vos images de grains de beauté facilement pour une analyse rapide.</p>
           </div>
@@ -30,9 +34,9 @@ const Home = () => {
         
         <div className="steps-section">
         <h2 className="steps-features-title">Découvrez comment utiliser notre service</h2>
-        <p className="steps-intro">Commencez par créer un compte pour accéder à toutes nos fonctionnalités. Une fois connecté, vous pourrez détecter la dangerosité de vos grains de beauté et suivre leur évolution.</p>
-
+        
         <div className="step">
+          <img src="/icon1.png" alt="-" className='icon' />
           <h3 className="step-title">Étape 1 : Créez votre compte</h3>
           <p className="step-text">Inscrivez-vous en quelques clics pour commencer.</p>
         </div>
@@ -40,6 +44,7 @@ const Home = () => {
         <div className="step-divider"></div>
 
         <div className="step">
+        <img src="/icon2.png" alt="-" className='icon' />
           <h3 className="step-title">Étape 2 : Téléchargez vos photos</h3>
           <p className="step-text">Glissez-déposez vos images de grains de beauté pour analyse.</p>
         </div>
@@ -47,19 +52,19 @@ const Home = () => {
         <div className="step-divider"></div>
 
         <div className="step">
+          
+        <img src="/icon3.png" alt="-" className='icon' />
           <h3 className="step-title">Étape 3 : Suivez l'évolution</h3>
           <p className="step-text">Organisez vos photos dans des albums pour un suivi facile.</p>
         </div>
       </div>
-      <div className="step-actions">
-            <button className="step-button unchecked">Commencer</button>
-            <button className="step-button checked">Se connecter</button>
-          </div>
 
       <div className="cta-section">
-        <h1 className="title">Créez votre compte gratuitement</h1>
-        <p className="text">Rejoignez notre communauté et commencez à surveiller la santé de vos grains de beauté. Essayez notre service innovant sans frais dès aujourd’hui !</p>
+        <h1 className="title-final">Créez votre compte gratuitement</h1>
+        <div className="cta-divider">
+        <p className="cta-text">Rejoignez notre communauté et commencez à surveiller la santé de vos grains de beauté. Essayez notre service innovant sans frais dès aujourd'hui !</p>
         <button className="button">Inscription</button>
+        </div>
       </div>
     </div>
   );
