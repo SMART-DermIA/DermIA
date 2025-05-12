c07ae16b504f36c261409e02140e4801b11b2bce
Xx-DarkBGdu69-xX <marcsauce02@gmail.com>
2025-05-12 18:29:03 +0200

    DB build avant de build le back

---
+++ Back/wait-for-postgres.sh
@@ -0,0 +1,15 @@
+- fichier wait-for-postgres.sh
+#!/bin/sh
+set -e
+
+HOST=${DB_HOST:-db}
+PORT=${DB_PORT:-5432}
+
+echo "⏳ Attente de la base de données à $HOST:$PORT..."
+
+until nc -z "$HOST" "$PORT"; do
+  sleep 1
+done
+
+echo "✅ Base de données prête, lancement de l'application..."
+exec "$@"
